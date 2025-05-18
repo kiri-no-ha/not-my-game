3920da2009b009c0adf78178779d86be9b22364d
chuhandorik <armenbogomolov08@mail.ru>
2025-05-18 14:50:29 +0300

    2222222222
    asdgfasdfg

--- not-my-game/Assets/Scripts/Traps/SlowTrap.cs
+++ not-my-game/Assets/Scripts/Traps/SlowTrap.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class SlowTrap : MonoBehaviour
 {
     // Start is called before the first frame update
+<<<<<<< Updated upstream
     private bool isSlow;
     public float slow;
     void Start()
     {
         isSlow = false;
+=======
+    public float slowing = 1;
+    private float start_speed;
+    void Start()
+    {
+        
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,12 +41,33 @@ public class SlowTrap : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision) 
     {
+=======
+        Debug.Log(start_speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+>>>>>>> Stashed changes
         if (collision != null)
         {
             if (collision.gameObject.tag == "Player")
             {
+<<<<<<< Updated upstream
                 collision.gameObject.GetComponent<PlayerMove>().ChangeSpeed(slow);
                 isSlow = false;
+=======
+                start_speed = collision.gameObject.GetComponent<PlayerMove>().speed;
+                collision.gameObject.GetComponent<PlayerMove>().speed /= slowing;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.gameObject.GetComponent<PlayerMove>().speed = start_speed;
+>>>>>>> Stashed changes
             }
         }
     }
